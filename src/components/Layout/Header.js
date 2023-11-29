3848e6ffb708aaca66a28e6841e5b3cd0ea02abd
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-11-29 20:57:59 +0530

    logout user

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,27 +1,49 @@
 import React, { useState } from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
 import Notifications from "./Notifications";
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const Header = (props) => {
-const [IsNotificationShown, setIsNotificationShown] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
-const showNotificationHandler = () => {
-  setIsNotificationShown(true);
-}
+  const [IsNotificationShown, setIsNotificationShown] = useState(false);
 
-const hideNotificationHandler = () => {
-  setIsNotificationShown(false);
-}
+  const showNotificationHandler = () => {
+    setIsNotificationShown(true);
+  };
+
+  const hideNotificationHandler = () => {
+    setIsNotificationShown(false);
+  };
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    history.replace('/');
+  };
 
   return (
-    <header style={{width: "100%"}}>
+    <header style={{ width: "100%" }}>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/home">Expense Tracker</Navbar.Brand>
-          <Button onClick={showNotificationHandler} className="rounded-circle">N</Button>
+          <Button onClick={showNotificationHandler} className="rounded-circle">
+            N
+          </Button>
+          {authCtx.isLoggedIn && (
+            <Button onClick={logoutHandler}>Logout</Button>
+          )}
         </Container>
       </Navbar>
-      {IsNotificationShown && <Notifications show={IsNotificationShown} onHide={hideNotificationHandler} />}
+
+      {IsNotificationShown && (
+        <Notifications
+          show={IsNotificationShown}
+          onHide={hideNotificationHandler}
+        />
+      )}
     </header>
   );
 };
